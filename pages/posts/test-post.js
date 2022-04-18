e7afa464251f7247398a93fc556be4faaf36cb49
matt-pilcher <mpilcher90@gmail.com>
2022-04-18 14:39:30 -0400

    add initial css config

--- pages/posts/test-post.js
+++ pages/posts/test-post.js
@@ -1,10 +1,14 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function TestPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>Test Post</title>
+      </Head>
       <h1>Test Post</h1>
-      <Link href='/'><a>Back to home</a></Link>
-    </>
+    </Layout>
   )
 }
